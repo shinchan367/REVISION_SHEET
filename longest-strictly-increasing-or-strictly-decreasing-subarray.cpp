af91d599377f1af1f06c1d32b60d7f870bf2b72d
Sanam manojna <98442984+shinchan367@users.noreply.github.com>
2025-02-03 21:49:17 +0530

    Create longest-strictly-increasing-or-strictly-decreasing-subarray.cpp

---
+++ longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -0,0 +1,26 @@
+lass Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+       int n=nums.size();
+       if(n==1){
+        return 1;
+       }
+
+       int inc=1,dec=1,maxlen=1;
+       for(int i=1;i<n;i++){
+        if(nums[i]>nums[i-1]){
+            inc++;
+            dec=1;
+        }
+        else if(nums[i]<nums[i-1]){
+              dec++;
+              inc=1;
+        }
+        else{
+        dec=inc=1;
+        }
+        maxlen=max(maxlen,max(inc,dec));
+       }
+       return maxlen;
+    }
+};
